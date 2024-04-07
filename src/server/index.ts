0e53205f196507fd0ab8a09e88eb9a6c9ac73648
Tony Hales <tony.hales@revlifter.com>
2024-04-08 00:06:06 +0100

    Added errors to server index.

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,6 +1,7 @@
 export { createServer, SocketApiServerProps, Server } from './ServerServer';
 export * from './ServerController';
 export * from './ServerStoreController';
+export * from '../common/errors';
 
 export { ControllerContext } from './ServerModels';
 export { executeWithThinClientContext } from './context';
