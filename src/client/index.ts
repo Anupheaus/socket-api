47c276c9b75fd508838b28189505be44d5e9747d
Tony Hales <tony.hales@revlifter.com>
2024-04-08 11:41:36 +0100

    Fixed issue with errors not being exported client side.

--- src/client/index.ts
+++ src/client/index.ts
@@ -3,5 +3,6 @@ import { ControllerMethodMetadata } from '../common/CommonModels';
 export * from './ControllersProvider';
 export * from './createUseController';
 export * from './useControllers';
+export * from '../common/errors';
 
 export { ControllerMethodMetadata };
